da0acb5a88aad43b6fee88256498970a84a54b31
abrar189 <algourabrar@gmail.com>
2021-08-03 13:20:21 +0300

    done

--- src/AllDataAPI.js
+++ src/AllDataAPI.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import { Card,Button } from 'react-bootstrap';
 
 
 class AllDataAPI extends Component {
+
+constructor(props){
+    super(props);
+    this.state={
+        dataColor:[],
+    }
+}
+
+// https://ltuc-asac-api.herokuapp.com/allColorData
+
+    componentDidMount = async ()=>{
+        let result= await axios.get(`http://localhost:3005/allColorData`)
+        this.setState({
+            dataColor:result.data,
+        })
+    }
+    addtofav =async(item)=>{
+        const dataObj={
+          email:this.props.auth0.user.email,
+          title:item.title,
+          imageUrl:item.imageUrl
+        };
+        await axios.post('http://localhost:3005/addtofav',dataObj)
+      }
     render() {
         return (
-            <div>
+            <>
                 <h1>All Data from the API</h1>
                 <h3>Select your favorites :)</h3>
-            </div>
+            {this.state.dataColor.map((item, index) => {
+          return (
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={item.imageUrl} alt="" />
+              <Card.Body>
+                <Card.Text>
+                 {item.title}
+                </Card.Text>
+                <Button variant="primary" onClick={()=>{this.addtofav(item)}}>Add to fav</Button>
+              </Card.Body>
+            </Card>
+            )
+        })}
+            </> 
         )
-    }
+       
+        }
 }
 
 export default withAuth0(AllDataAPI);
--- src/MyFavorites.js
+++ src/MyFavorites.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './MyFavorites.css';
+import axios from 'axios';
+import { Card,Button } from 'react-bootstrap';
 import { withAuth0 } from '@auth0/auth0-react';
 
 class MyFavorites extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colorData: [],
+      index:0,
+    }
+  }
+  // http://localhost:3005/datafromdb
+  componentDidMount = async () => {
+    
+    let { email } = this.props.auth0.user
+    let result = await axios.get(`http://localhost:3005/datafromdb?email=${email}`)
+    this.setState({
+      colorData: result.data
+    })
+    console.log(this.state.colorData);
+  }
+
+  deletCard = async (index)=>{
+    let paramObj ={
+      email:this.props.auth0.user.email
+    }
+    let resdelet=await axios.delete(`http://localhost:3005/delete/${index}`,{params:paramObj});
+    this.setState({
+      colorData:resdelet.data,
+      index:index
+    })
+  }
   render() {
-    return(
+    return (
       <>
         <h1>My Favorites</h1>
         <p>
           This is a collection of my favorites
         </p>
+        {this.state.colorData.map((item, index) => {
+          return (
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={item.imageUrl} alt="" />
+              <Card.Body>
+                <Card.Text>
+                 {item.title}
+                </Card.Text>
+                {/* <Button variant="primary" onClick={()=>{this.addtofav(item)}}>Add to fav</Button> */}
+              </Card.Body>
+            </Card>
+          )
+        })}
       </>
+
     )
   }
 }
